b9e312774b274a866db7d7215dfc94f861f94555
papoudaa <argirispapoudakis@gmail.com>
2022-07-11 12:36:31 +0300

    update models package

--- models/train.py
+++ models/train.py
@@ -1,9 +1,14 @@
 from torch.utils.data import DataLoader
 import torch
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import torch.nn as nn
 
 
-def train_loop(model, train_data, test_data, batch_size, num_epochs, device, optimizer, criterion):
+def train_loop(model, train_data, test_data, batch_size, num_epochs, device):
 
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    criterion = nn.CrossEntropyLoss()
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
     model = model.to(device)
@@ -15,7 +20,7 @@ def train_loop(model, train_data, test_data, batch_size, num_epochs, device, opt
         model.train()
         train_epoch_losses = []
         test_epoch_losses = []
-        for it, data in enumerate(train_loader):
+        for it, data in enumerate(tqdm(train_loader)):
             x, labels = data
             x = x.to(device)
             labels = labels.to(device)
@@ -32,16 +37,22 @@ def train_loop(model, train_data, test_data, batch_size, num_epochs, device, opt
             train_epoch_losses.append(loss.item())
 
         with torch.no_grad():
-            for test_it, data in enumerate(test_loader):
+            for test_it, data in enumerate(tqdm(test_loader)):
                 test_x, test_labels = data
                 test_x = test_x.to(device)
                 test_labels = test_labels.to(device)
 
                 test_outputs = model(test_x)
-                loss = criterion(test_outputs, test_labels)
-                test_epoch_losses.append(loss.item())
+                test_loss = criterion(test_outputs, test_labels)
+                test_epoch_losses.append(test_loss.item())
 
         train_losses.append(sum(train_epoch_losses)/len(train_epoch_losses))
         test_losses.append(sum(test_epoch_losses)/len(test_epoch_losses))
 
         print(f"Epoch: {epoch+1} Train Loss: {train_losses[epoch]} Test Loss: {test_losses[epoch]}")
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.plot(train_losses)
+    plt.plot(test_losses)
+    plt.show()
