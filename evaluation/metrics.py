6239f9ede341c3b09eb483bc80339a91af407931
papoudaa <argirispapoudakis@gmail.com>
2022-07-13 13:49:30 +0300

    update metrics

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -4,3 +4,15 @@ def accuracy(outputs, targets):
     num_correct = (pred == targets).float().sum()
     acc = (num_correct/targets.shape[0]).item()
     return acc
+
+
+def precision(num_correct, num_preds):
+    return num_correct/num_preds
+
+
+def recall(num_correct, num_gt):
+    return num_correct/num_gt
+
+
+def f1_score(precision, recall):
+    return 2*(precision*recall)/(precision+recall)
