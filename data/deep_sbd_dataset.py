9f85d55d4025157e53e3ef7a4a01e958bef75143
papoudaa <argirispapoudakis@gmail.com>
2022-08-09 12:18:31 +0300

    add width height arguments

--- data/deep_sbd_dataset.py
+++ data/deep_sbd_dataset.py
@@ -6,7 +6,7 @@ from utils.video import array_to_video
 import pandas as pd
 
 
-def data_formatting(root_dir, annotation_path):
+def data_formatting(root_dir, annotation_path, width, height):
 
     data_segments = ["tv7789", "tv2001", "tv2007t", "tv2007d", "tv2008", "tv2009"]
     idx = 0
@@ -21,7 +21,7 @@ def data_formatting(root_dir, annotation_path):
                 frames = []
                 files = [x for x in files if x.endswith(".jpg")]
                 for f in sorted(files, key=lambda x: list(map(int, x.split(".")[0]))):
-                    frames.append(asarray(Image.open(os.path.join(r, f)).resize((64, 64))))
+                    frames.append(asarray(Image.open(os.path.join(r, f)).resize((width, height))))
                 video = np.stack(frames)
                 folders = r.split("/")
                 if "graduals" in folders:
@@ -38,4 +38,4 @@ def data_formatting(root_dir, annotation_path):
     df.to_csv(os.path.join(annotation_path, "annotations.csv"), mode="a", index=False, header=False)
 
 
-data_formatting("../../Data/DeepSBD/", "../../Data/deepSBD/EditedDeepSBD/")
+data_formatting("../../Data/DeepSBD/", "../../Data/deepSBD/EditedDeepSBD/", 112, 112)
