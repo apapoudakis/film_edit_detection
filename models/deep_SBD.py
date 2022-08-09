0c52c5839b0d72ad8539508f0def5c49a247c0fa
papoudaa <argirispapoudakis@gmail.com>
2022-08-09 14:21:23 +0300

    rewrite deepSBD model

--- models/deep_SBD.py
+++ models/deep_SBD.py
@@ -1,6 +1,5 @@
 """
 deepSBD model https://arxiv.org/abs/1705.03281
-Implementations is from https://github.com/Tangshitao/ClipShots_basline
 """
 import torch.nn as nn
 import cv2
@@ -10,51 +9,42 @@ import utils.video
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv3d(3, 96, kernel_size=3, stride=(1, 2, 2),
-                               padding=(0, 0, 0), bias=True)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=0)
-        self.conv2 = nn.Conv3d(96, 256, kernel_size=3, stride=(1, 2, 2),
-                               padding=(0, 0, 0), bias=True)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=0)
-        self.conv3 = nn.Conv3d(256, 384, kernel_size=3, stride=1,
-                               padding=1, bias=True)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.conv4 = nn.Conv3d(384, 384, kernel_size=(3, 3, 3), stride=1,
-                               padding=1, bias=True)
-        self.relu4 = nn.ReLU(inplace=True)
-        self.conv5 = nn.Conv3d(384, 256, kernel_size=(3, 3, 3), stride=1,
-                               padding=1, bias=True)
-        self.relu5 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=0)
-        self.fc6 = nn.Linear(18432, 2048)
-        self.relu6 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv3d(in_channels=3, out_channels=96, kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=(0, 0, 0))
+        self.conv1_bn = nn.BatchNorm3d(96)  # instead of Local Response Normalization (LRN)
+        self.max_pool1 = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=(1, 2, 2))
+        self.conv2 = nn.Conv3d(in_channels=96, out_channels=256, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 2, 2))
+        self.conv2_bn = nn.BatchNorm3d(256)  # instead of Local Response Normalization (LRN)
+        self.max_pool2 = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=(1, 2, 2))
+        self.conv3 = nn.Conv3d(in_channels=256, out_channels=384, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
+        self.conv4 = nn.Conv3d(in_channels=384, out_channels=384, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
+        self.conv5 = nn.Conv3d(in_channels=384, out_channels=256, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
+        self.max_pool5 = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=(0, 1, 1))
+        self.fc6 = nn.Linear(8*7*7*256, 2048)
         self.fc7 = nn.Linear(2048, 2048)
-        self.relu7 = nn.ReLU(inplace=True)
         self.fc8 = nn.Linear(2048, 3)
+        self.dropout = nn.Dropout(p=0.5)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = self.conv4(x)
-        x = self.relu4(x)
-        x = self.conv5(x)
-        x = self.relu5(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc6(x)
-        x = self.relu6(x)
-        x = self.fc7(x)
-        x = self.relu7(x)
-        x = self.fc8(x)
+        h = self.relu(self.conv1_bn(self.conv1(x)))
+        h = self.max_pool1(h)
 
-        return x
+        h = self.relu(self.conv2_bn(self.conv2(h)))
+        h = self.max_pool2(h)
+
+        h = self.relu(self.conv3(h))
+        h = self.relu(self.conv4(h))
+        h = self.relu(self.conv5(h))
+        h = self.max_pool5(h)
+
+        h = h.view(h.size(0), -1)
+        h = self.relu(self.fc6(h))
+        h = self.dropout(h)
+        h = self.relu(self.fc7(h))
+        h = self.dropout(h)
+        logits = self.fc8(h)
+
+        return logits
 
 
 def post_processing(img1, img2, threshold):
